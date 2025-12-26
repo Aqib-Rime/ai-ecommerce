29a815e2db2c027fe002c29f812163fb48cbd41d
Aqib <aqibrime@gmail.com>
2025-12-26 11:34:45 +0600

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typedRoutes: true,
+  cacheComponents: true,
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    inlineCss: true,
+  },
   /* config options here */
 };
 
